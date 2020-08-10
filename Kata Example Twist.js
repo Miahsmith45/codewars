181c85007bee7774123aad91aac60ad508e37a0b
Susan Smith <veryjeremiah@gmail.com>
2020-08-10 10:45:08 -0600

    adding katas

---
+++ Kata Example Twist.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031
+var websites = []
+for (let index = 0; index < 1000; index++) {
+    websites.push("codewars")
+
+}
+/// best answer
+var websites = new Array(1000).fill("codewars");
