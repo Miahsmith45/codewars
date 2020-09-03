8fcdfcf4974c1be037bc945a8d3bc61fa0df4f01
Susan Smith <veryjeremiah@gmail.com>
2020-09-03 00:29:36 -0600

    d

---
+++ Regular Ball Super Ball.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53f0f358b9cb376eca001079
+var Ball = function(ballType = 'regular') {
+    this.balltype = ballType
+};
+var Ball = function(ballType = "regular") {
+    this.ballType = ballType;
+};
---
+++ You Can't Code Under Pressure #1.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i + i;
+}
+
+function doubleInteger(i) {
+    return i * 2
+}
